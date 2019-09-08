bcc5b41de62dd82fe6a44ffe10c5f340dd51f5f0
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 13:58:09 -0400

    Next file_reader test passed (First row of puzzle grid contains the correct characters)

--- test/models/test_file_reader.py
+++ test/models/test_file_reader.py
@@ -8,3 +8,10 @@ def test_file_reader__words_to_find_list_should_contain_BIGHOUSE_GOBLUE_HAIL_MIC
     assert words_to_find == ['BIGHOUSE', 'GOBLUE', 'HAIL', 'MICHIGAN', 'WOLVERINES']
 
 
+def test_file_reader__first_row_of_puzzle_grid_should_contain_the_correct_characters():
+    words_to_find, puzzle_grid = file_reader()
+
+    assert len(puzzle_grid[0]) == 15
+    assert puzzle_grid[0] == ['S', 'G', 'C', 'W', 'J', 'J', 'C', 'D', 'B', 'C', 'R', 'J', 'P', 'T', 'W']
+
+
