2d5b77ba45ca16474d218ed619522714232bb7c2
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 15:13:50 -0400

    Next test of apply_move_to_coordinates passed (x was advanced by -1, y remained the same as per the given move)

--- test/models/test_utilities.py
+++ test/models/test_utilities.py
@@ -10,3 +10,14 @@ def test_apply_move_to_coordinates__should_advance_coordinates_by_1():
 
     assert new_x == 3
     assert new_y == 3
+
+
+def test_apply_move_to_coordinates__should_advance_x_coord_by_minus_1_y_should_remain_the_same():
+    temp_x = 2
+    temp_y = 2
+    move = (-1, 0)
+
+    new_x, new_y = apply_move_to_coordinates(temp_x, temp_y, move)
+
+    assert new_x == 1
+    assert new_y == 2
