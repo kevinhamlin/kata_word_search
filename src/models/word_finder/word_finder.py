7a095c66183c710e69b8d59acbe049353a0a3a2a
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 14:37:04 -0400

    Updated search_row_for_y_coord to handle case mismatch.

--- src/models/word_finder/word_finder.py
+++ src/models/word_finder/word_finder.py
@@ -3,6 +3,6 @@
 
 def search_row_for_y_coord(row, character):
     for y, unknown_character in enumerate(row):
-        if unknown_character == character:
+        if unknown_character.lower() == character.lower():
             return y
     return None
