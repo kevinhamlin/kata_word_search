fb46486f1a65d9e17ad1a761342d416cca35ba8b
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 13:56:46 -0400

    Refactored file_reader into multiple smaller, more specific/modular functions.

--- src/models/file_reader/file_reader.py
+++ src/models/file_reader/file_reader.py
@@ -2,20 +2,26 @@ import os
 
 
 def file_reader():
-    grid_list = []
     models_dir = os.path.dirname(os.path.dirname(__file__))
     input_filepath = os.path.join(models_dir, "input", "input.txt")
 
     with open(input_filepath, "r") as infile:
-        for index, line in enumerate(infile):
-            if index == 0:
-                temp_string = line.strip('\n')
-                words_to_find = temp_string.split(',')
-            else:
-                temp_grid_string = line.strip('\n')
-                row = temp_grid_string.split(',')
-                grid_list.append(row)
+        return parse_file(infile)
 
-    result = (words_to_find, grid_list)
-    return result
+
+def parse_file(infile):
+    grid_list = []
+    words_to_find = []
+
+    for index, line in enumerate(infile):
+        if index == 0:
+            words_to_find = scrub_row(line)
+        else:
+            grid_list.append(scrub_row(line))
+    return words_to_find, grid_list
+
+
+def scrub_row(line):
+    row = line.strip('\n').split(',')
+    return row
 
