226a2805ddbaa90027815858128b2fe711515f70
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 13:47:00 -0400

    Corrected typo in test name

--- test/models/test_file_reader.py
+++ test/models/test_file_reader.py
@@ -1,7 +1,7 @@
 from src.models.file_reader.file_reader import file_reader
 
 
-def test_filewords_to_find_list_should_contain_BIGHOUSE_GOBLUE_HAIL_MICHIGAN_WOLVERINES():
+def test_file_reader__words_to_find_list_should_contain_BIGHOUSE_GOBLUE_HAIL_MICHIGAN_WOLVERINES():
     words_to_find, puzzle_grid = file_reader()
 
     assert len(words_to_find) == 5
