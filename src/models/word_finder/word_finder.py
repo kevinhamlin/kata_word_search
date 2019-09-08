49bf24c978815d4665d120ef6b678525d8b32a7d
Amplexis <kevin.hamlin@gmail.com>
2019-09-08 14:46:23 -0400

    Added find_first_letter_coords function.

--- src/models/word_finder/word_finder.py
+++ src/models/word_finder/word_finder.py
@@ -6,3 +6,11 @@ def search_row_for_y_coord(row, character):
         if unknown_character.lower() == character.lower():
             return y
     return None
+
+
+def find_first_letter_coords(character, grid):
+    for x, row in enumerate(grid):
+        result = search_row_for_y_coord(row, character)
+
+        if result is not None:
+            return x, result
