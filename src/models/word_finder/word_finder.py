5179db8b2d84ffa4aabdc0b85fc6f09cfce6318d
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 13:04:46 -0400

    Removed unnecessary functions search_row_for_y_coord, and find_first_letter_coords. Functions replaced by search_grid_for_instances_of_first_letter.

--- src/models/word_finder/word_finder.py
+++ src/models/word_finder/word_finder.py
@@ -13,21 +13,6 @@ def search_grid_for_all_instances_of_first_letter(grid, letter):
     return None
 
 
-def search_row_for_y_coord(row, character):
-    for y, unknown_character in enumerate(row):
-        if unknown_character.lower() == character.lower():
-            return y
-    return None
-
-
-def find_first_letter_coords(character, grid):
-    for x, row in enumerate(grid):
-        result = search_row_for_y_coord(row, character)
-
-        if result is not None:
-            return x, result
-
-
 def find_possible_second_letter_coords(coords):
     x, y = coords
     possible_moves = []
