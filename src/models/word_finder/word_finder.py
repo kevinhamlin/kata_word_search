63f2f4cbeae199a8116583636f175ba7e22cc211
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 14:00:33 -0400

    Renamed find_rest_of_word and refactored into 2 functions.

--- src/models/word_finder/word_finder.py
+++ src/models/word_finder/word_finder.py
@@ -39,17 +39,23 @@ def find_rest_of_word(matched_coord, move_used, word, grid):
     temp_x, temp_y = matched_coord
     while letter_index < len(word) and match:
         temp_x, temp_y = apply_move_to_coordinates(temp_x, temp_y, move_used)
-        if range_check(temp_x, temp_y):
-            if grid[temp_x][temp_y].lower() == word[letter_index].lower():
-                remaining_coords.append((temp_x, temp_y))
-                letter_index += 1
-            else:
-                match = False
-        else:
-            match = False
+        letter_index, match = compare_remaining_letters(grid, letter_index, match, remaining_coords, temp_x, temp_y,
+                                                        word)
     if match:
         return remaining_coords
     else:
         return None
+
+
+def compare_remaining_letters(grid, letter_index, match, remaining_coords, temp_x, temp_y, word):
+    if range_check(temp_x, temp_y):
+        if grid[temp_x][temp_y].lower() == word[letter_index].lower():
+            remaining_coords.append((temp_x, temp_y))
+            letter_index += 1
+        else:
+            match = False
+    else:
+        match = False
+    return letter_index, match
 
 
