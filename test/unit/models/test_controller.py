a964216714609a59f667948fae4cc3ff9905332e
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 18:20:31 -0400

    Passed test for call to find_second_letter_matches (called once).

--- test/unit/models/test_controller.py
+++ test/unit/models/test_controller.py
@@ -4,7 +4,7 @@ from src.models.controller.controller import begin_search
 
 
 @patch('src.models.controller.controller.search_grid_for_all_instances_of_first_letter')
-def test_word_finder__should_call_search_grid_for_instance_of_first_letter_once(mock_validate):
+def test_begin_search__should_call_search_grid_for_instance_of_first_letter_once(mock_validate):
     words_to_find = ["Big"]
     puzzle_grid = [
         ["B", "I", "G", "D"],
@@ -18,4 +18,16 @@ def test_word_finder__should_call_search_grid_for_instance_of_first_letter_once(
     mock_validate.assert_called_with(puzzle_grid, "B")
 
 
+@patch('src.models.controller.controller.find_second_letter_matches')
+@patch('src.models.controller.controller.search_grid_for_all_instances_of_first_letter')
+def test_begin_search__should_call_search_grid_for_instance_of_first_letter_once(mock_validate, mock_matches):
+    words_to_find = ["Big"]
+    puzzle_grid = [
+        ["B", "I", "G", "D"],
+        ["A", "I", "I", "G"],
+        ["G", "T", "X", "P"],
+    ]
 
+    begin_search(words_to_find, puzzle_grid)
+
+    assert mock_matches.call_count == 1
