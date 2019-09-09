c38beaeb97623fed7ac442f1181104ba90974388
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 18:56:10 -0400

    Passed get_matched_second_letter_coords test (search_surrounding_spaces_for_second_letter called once with correct parameters).

--- test/unit/models/test_controller.py
+++ test/unit/models/test_controller.py
@@ -1,6 +1,6 @@
 from mock import patch
 
-from src.models.controller.controller import begin_search, find_second_letter_matches
+from src.models.controller.controller import begin_search, find_second_letter_matches, get_matched_second_letter_coords
 
 
 @patch('src.models.controller.controller.search_grid_for_all_instances_of_first_letter')
@@ -46,3 +46,19 @@ def test_find_second_letter_matches__should_call_find_possible_second_letter_coo
 
     assert mock_validate.call_count == 1
     mock_validate.assert_called_with((0, 0))
+
+
+@patch('src.models.controller.controller.search_surrounding_spaces_for_second_letter')
+def test_get_matched_second_letter_coords__should_call_search_surrounding_spaces_for_second_letter_once(mock_validate):
+    letter = "I"
+    possible_moves = [(0, 1), (1, 1)]
+    puzzle_grid = [
+        ["B", "I", "G", "D"],
+        ["A", "I", "I", "G"],
+        ["G", "T", "X", "P"],
+    ]
+
+    get_matched_second_letter_coords(letter, possible_moves, puzzle_grid)
+
+    assert mock_validate.call_count == 1
+    mock_validate.assert_called_with(letter, possible_moves, puzzle_grid)
