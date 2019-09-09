0ed10f43ff0c2af15978c558308393638fd13fac
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 15:49:50 -0400

    Second compare_remaining_letters test passed (returns true when letters match).

--- test/models/test_word_finder.py
+++ test/models/test_word_finder.py
@@ -319,4 +319,25 @@ def test_compare_remaining_letters__match_should_return_false_when_letters_do_no
     assert letter_index == 2
 
 
+def test_compare_remaining_letters__match_should_return_true_when_letters_match():
+    grid = [
+        ["B", "I", "R", "D"],
+        ["A", "I", "I", "G"],
+        ["G", "T", "X", "P"],
+    ]
+    letter_index = 2
+    match = True
+    remaining_coords = []
+    temp_x = 0
+    temp_y = 2
+    word = "BIRD"
+
+    letter_index, match = compare_remaining_letters(grid, letter_index, match, remaining_coords, temp_x, temp_y, word)
+
+
+    assert match is True
+    assert letter_index == 3
+
+
+
 
