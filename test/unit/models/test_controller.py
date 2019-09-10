d524749d59fc70214c6876cb5f5205acf03fc2b0
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 20:30:03 -0400

    get_move_used passed test for call to find_move_used.

--- test/unit/models/test_controller.py
+++ test/unit/models/test_controller.py
@@ -1,6 +1,7 @@
 from mock import patch
 
-from src.models.controller.controller import begin_search, find_second_letter_matches, get_matched_second_letter_coords
+from src.models.controller.controller import begin_search, find_second_letter_matches, get_matched_second_letter_coords, \
+    get_move_used
 
 
 @patch('src.models.controller.controller.search_grid_for_all_instances_of_first_letter')
@@ -62,3 +63,15 @@ def test_get_matched_second_letter_coords__should_call_search_surrounding_spaces
 
     assert mock_validate.call_count == 1
     mock_validate.assert_called_with(letter, possible_moves, puzzle_grid)
+
+
+@patch('src.models.controller.controller.find_move_used')
+def test_get_move_used__should_call_find_move_used_twice(mock_validate):
+    matches = [(0, 1), (1, 1)]
+    coord_to_check = (0, 0)
+
+    get_move_used(matches, coord_to_check)
+
+    assert mock_validate.call_count == 2
+
+
