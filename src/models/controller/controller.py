ff1ecd2a5380a3adf2df338f3c074be27199e535
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 21:29:52 -0400

    Added create_list_of_found_word_coords.

--- src/models/controller/controller.py
+++ src/models/controller/controller.py
@@ -1,3 +1,4 @@
+from src.models.utilities.build_final_list_of_coords import build_final_list_of_coords
 from src.models.utilities.find_move_used import find_move_used
 from src.models.word_finder.word_finder import search_grid_for_all_instances_of_first_letter, \
     find_possible_second_letter_coords, search_surrounding_spaces_for_second_letter, search_remaining_letters_in_a_line
@@ -24,8 +25,14 @@ def get_matched_second_letter_coords(letter, possible_moves, puzzle_grid, coord_
 def get_move_used(matches, coord_to_check, word, puzzle_grid):
     for match in matches:
         move_used = find_move_used(coord_to_check, match)
-        check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid)
+        check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid, coord_to_check)
 
 
-def check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid):
+def check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid, coord_to_check):
     result = search_remaining_letters_in_a_line(match, move_used, word, puzzle_grid)
+    if result is not None:
+        create_list_of_found_word_coords(result, coord_to_check, match)
+
+
+def create_list_of_found_word_coords(result, coord_to_check, match):
+    final_list_of_word_coords = build_final_list_of_coords(coord_to_check, match, result)
