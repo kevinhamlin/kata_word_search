4a743b91f861c7eafcbba506273b0746c18d37bd
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 20:46:48 -0400

    Added check_if_path_matches_rest_of_word.

--- src/models/controller/controller.py
+++ src/models/controller/controller.py
@@ -1,5 +1,6 @@
+from src.models.utilities.find_move_used import find_move_used
 from src.models.word_finder.word_finder import search_grid_for_all_instances_of_first_letter, \
-    find_possible_second_letter_coords
+    find_possible_second_letter_coords, search_surrounding_spaces_for_second_letter, search_remaining_letters_in_a_line
 
 
 def begin_search(words_to_find, puzzle_grid):
@@ -12,3 +13,19 @@ def begin_search(words_to_find, puzzle_grid):
 def find_second_letter_matches(first_letter_coords, word, puzzle_grid):
     for coord_to_check in first_letter_coords:
         possible_moves = find_possible_second_letter_coords(coord_to_check)
+        get_matched_second_letter_coords(word[1], possible_moves, puzzle_grid, coord_to_check, word)
+
+
+def get_matched_second_letter_coords(letter, possible_moves, puzzle_grid, coord_to_check, word):
+    matches = search_surrounding_spaces_for_second_letter(letter, possible_moves, puzzle_grid)
+    get_move_used(matches, coord_to_check, word, puzzle_grid)
+
+
+def get_move_used(matches, coord_to_check, word, puzzle_grid):
+    for match in matches:
+        move_used = find_move_used(coord_to_check, match)
+        check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid)
+
+
+def check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid):
+    result = search_remaining_letters_in_a_line(match, move_used, word, puzzle_grid)
