479ec4ca633d270f5078b90bde18bb9a3fcb4932
Amplexis <kevin.hamlin@gmail.com>
2019-09-10 11:27:17 -0400

    Removed duplicate test_find_second_letter_matches__should_call_find_possible_second_letter_coords_once test. All tests pass.

--- test/unit/models/test_controller.py
+++ test/unit/models/test_controller.py
@@ -19,20 +19,6 @@ def test_begin_search__should_call_search_grid_for_instance_of_first_letter_once
     mock_validate.assert_called_with(puzzle_grid, "B")
 
 
-@patch('src.models.controller.controller.find_second_letter_matches')
-def test_begin_search__should_call_search_grid_for_instance_of_first_letter_once(mock_matches):
-    words_to_find = ["Big"]
-    puzzle_grid = [
-        ["B", "I", "G", "D"],
-        ["A", "I", "I", "G"],
-        ["G", "T", "X", "P"],
-    ]
-
-    begin_search(words_to_find, puzzle_grid)
-
-    assert mock_matches.call_count == 1
-
-
 @patch('src.models.controller.controller.find_possible_second_letter_coords')
 def test_find_second_letter_matches__should_call_find_possible_second_letter_coords_once(mock_validate):
     first_letter_coords = [(0, 0)]
