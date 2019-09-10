6bdd127e11ae8279f8545fc41acf6404e24a78cc
Amplexis <kevin.hamlin@gmail.com>
2019-09-09 23:37:02 -0400

    Updated all functions to return final_output_string or None as appropriate.

--- src/models/controller/controller.py
+++ src/models/controller/controller.py
@@ -5,39 +5,54 @@ from src.models.word_finder.word_finder import search_grid_for_all_instances_of_
     find_possible_second_letter_coords, search_surrounding_spaces_for_second_letter, search_remaining_letters_in_a_line
 
 
-def begin_search(words_to_find, puzzle_grid):
+def begin_search(words_to_find, puzzle_grid, final_output_list):
     for word in words_to_find:
         letter = word[0]
         first_letter_coords = search_grid_for_all_instances_of_first_letter(puzzle_grid, letter)
-        find_second_letter_matches(first_letter_coords, word, puzzle_grid)
+        final_output_string = __find_second_letter_matches(first_letter_coords, word, puzzle_grid)
+        if final_output_string is not None:
+            final_output_list.append(final_output_string)
+    return final_output_list
 
 
-def find_second_letter_matches(first_letter_coords, word, puzzle_grid):
+def __find_second_letter_matches(first_letter_coords, word, puzzle_grid):
     for coord_to_check in first_letter_coords:
         possible_moves = find_possible_second_letter_coords(coord_to_check)
-        get_matched_second_letter_coords(word[1], possible_moves, puzzle_grid, coord_to_check, word)
+        final_output_string = get_matched_second_letter_coords(word[1], possible_moves, puzzle_grid, coord_to_check, word)
+        if final_output_string is not None:
+            return final_output_string
+    return None
 
 
 def get_matched_second_letter_coords(letter, possible_moves, puzzle_grid, coord_to_check, word):
     matches = search_surrounding_spaces_for_second_letter(letter, possible_moves, puzzle_grid)
-    get_move_used(matches, coord_to_check, word, puzzle_grid)
+    final_output_string = get_move_used(matches, coord_to_check, word, puzzle_grid)
+    if final_output_string is not None:
+        return final_output_string
+    return None
 
 
 def get_move_used(matches, coord_to_check, word, puzzle_grid):
     for match in matches:
         move_used = find_move_used(coord_to_check, match)
-        check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid, coord_to_check)
+        final_output_string = check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid, coord_to_check)
+        if final_output_string is not None:
+            return final_output_string
+    return None
 
 
 def check_if_path_matches_rest_of_word(match, move_used, word, puzzle_grid, coord_to_check):
     result = search_remaining_letters_in_a_line(match, move_used, word, puzzle_grid)
     if result is not None:
-        create_list_of_found_word_coords(result, coord_to_check, match, word)
+        final_output_string = create_list_of_found_word_coords(result, coord_to_check, match, word)
+        return final_output_string
+    return None
 
 
 def create_list_of_found_word_coords(result, coord_to_check, match, word):
     final_list_of_word_coords = build_final_list_of_coords(coord_to_check, match, result)
-    get_output_string(word, final_list_of_word_coords)
+    final_output_string = get_output_string(word, final_list_of_word_coords)
+    return final_output_string
 
 
 def get_output_string(word, final_list_of_word_coords):
